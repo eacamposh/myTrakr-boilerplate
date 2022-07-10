1ee86d275b4027bca48d8e6bc3488c2d36d4568c
eacamposh <eacamposh@gmail.com>
2022-07-09 18:57:38 -0700

    Add each element of the transactions' list to the table using jQuery.

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -157,14 +157,14 @@ $(document).ready(function () {
   function showSelected(e) {
     console.log(e);
     if (this.checked) {
-      if (this.id === 'input-withdraw' || this.id === 'input-deposit' ) {
+      if (this.id === 'input-withdraw' || this.id === 'input-deposit') {
 
         console.log('with or deposit');
         $('select[name=select-from]').attr("disabled", true);
         $('select[name=select-to]').attr("disabled", true);
         $('select[name=select-account]').attr("disabled", false);
-      } 
-      else if(this.id === 'input-transfer' ) {
+      }
+      else if (this.id === 'input-transfer') {
         console.log('ELSE:');
         $('select[name=select-from]').attr("disabled", false);
         $('select[name=select-to]').attr("disabled", false);
@@ -181,94 +181,94 @@ $(document).ready(function () {
 
 
     // $('input#radio1').click(function(e){
-  
+
     // //alert('id ='+ e.currentTarget.id);
     // console.log('id ='+ e.currentTarget.id);
-  
+
     // });
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
     //Start coding here!
     // Eduardo Campos
   });
-  
-  
-  
+
+
+
   $(document).ready(function () {
-  
-  
+
+
     let accountsArray = [];
     const empty = 'select-account';
-  
+
     $('#btn-add-new-category').attr('disabled', true);
     $('#input-new-category').attr('disabled', true);
     //dummy section while waiting merge.
-  
-  
-  
-  
+
+
+
+
     // to populate category select when the page is loading.
     getCategories();
-  
+
     $('select#select-category').click(function () {
-  
+
       let aux = $("#select-category").val();
       console.log('value' + aux);
       if (aux === 'Add new') {
-  
+
         console.log('inner function papa');
-  
+
         $("#input-new-category").val('');
-  
+
         $('#btn-add-new-category').attr('disabled', false);
         $('#input-new-category').attr('disabled', false);
-  
-  
+
+
       } else {
-  
-  
+
+
         $('#btn-add-new-category').attr('disabled', true);
         $('#input-new-category').attr('disabled', true);
       }
-  
-  
+
+
     });
-  
+
     $("#btn-add-new-category").on("click", function () {
-  
-  
+
+
       let category_input = $('#input-new-category').val();
       console.log(category_input);
-  
+
       if (category_input != '') {
-  
+
         saveNewCategory(category_input);
         getCategories();
-  
+
         //it should add the new category
         $("#errorMessage").text('');
         $("#succesMessage").text('A new category was added');
-  
+
         $('#btn-add-new-category').attr('disabled', true);
         $('#input-new-category').attr('disabled', true);
-  
+
       } else {
         $("#errorMessage").text('error, you have to type a category name to add');
         $("#succesMessage").text('');
       }
-  
+
     });
-  
-  
+
+
     function saveNewCategory(input) {
-  
-  
+
+
       console.log('category ql ' + input);
-  
+
       const data = { newCategory: input, };
       $.ajax({
         method: 'post',
@@ -277,16 +277,16 @@ $(document).ready(function () {
         dataType: 'json',
       }).done((data) => {
         console.log('data post ajax', data);
-  
+
       });
-  
-  
+
+
     }
-  
+
     function getCategories() {
       const new_category = 'Add new';
       const select = 'Select Category';
-  
+
       $.ajax({
         method: 'get',
         url: 'http://localhost:3000/categories',
@@ -294,32 +294,32 @@ $(document).ready(function () {
       }).done((data) => {
         console.log('data get ajax', data);
         $('select#select-category').empty();
-  
+
         $('select#select-category').append('<option>' + select + '</option>');
-  
+
         $.each(data, (i, post) => {
           $('select#select-category').append('<option>' + post.name + '</option>'
           );
         });
         $('select#select-category').append('<option>' + new_category + '</option>');
-  
-  
+
+
       });
-  
+
     }
-  
+
     // radio button toggle
     // add an event listener for the change event
     const radioButtons = document.querySelectorAll('input[name="transaction-type"]');
     for (const radioButton of radioButtons) {
       radioButton.addEventListener('change', showSelected);
     }
-  
+
     function showSelected(e) {
       console.log(e);
       if (this.checked) {
         if (this.id === 'input-withdraw' || this.id === 'input-deposit') {
-  
+
           console.log('with or deposit');
           $('select[name=select-from]').attr("disabled", true);
           $('select[name=select-to]').attr("disabled", true);
@@ -330,18 +330,18 @@ $(document).ready(function () {
           $('select[name=select-from]').attr("disabled", false);
           $('select[name=select-to]').attr("disabled", false);
           $('select[name=select-account]').attr("disabled", true);
-  
+
         }
         console.log('you have selected :' + this.id);
-  
+
       }
     }
-  
+
     //? Eduardo's Code
-  
+
     getNewAccounts();
-  
-  
+
+
     $('#btn-add-new-account').on('click', function (e) {
       let new_account = $('#new-account').val();
       if (new_account.length == 0) {
@@ -353,7 +353,7 @@ $(document).ready(function () {
         $('#select-from').append('<option>' + new_account + '</option>');
         $('#select-to').append('<option>' + new_account + '</option>');
         $('#select-filter-by-account').append('<option>' + new_account + '</option>');
-  
+
         $.ajax({
           method: 'post',
           data: JSON.stringify({
@@ -370,7 +370,7 @@ $(document).ready(function () {
         });
       }
     });
-  
+
     function getNewAccounts() {
       let new_account = "Select-Option";
       $.ajax({
@@ -384,51 +384,51 @@ $(document).ready(function () {
         $('#select-to').empty();
         $('#select-filter-by-account').empty();
         console.log('Select-account', new_account);
-  
+
         $('#select-account').append('<option>' + new_account + '</option>');
         $('#select-from').append('<option>' + new_account + '</option>');
         $('#select-to').append('<option>' + new_account + '</option>');
         $('#select-filter-by-account').append('<option>' + new_account + '</option>');
-  
-  
+
+
         $.each(data, (i, post) => {
           console.log('each: ', post.username);
           $('#select-account').append('<option>' + post.username + '</option>');
           $('#select-from').append('<option>' + post.username + '</option>');
           $('#select-to').append('<option>' + post.username + '</option>');
           $('#select-filter-by-account').append('<option>' + post.username + '</option>');
-  
+
         });
       });
-  
+
     }
-  
+
     //getJSON - file, cb(data)
     $.getJSON('data.json', (data) => {
       console.log('data json', data);
       $.each(data, (index, user) => {
-        $('#list').append(`
-                  <li>
-                   ${user.id}
-                   ${user.username}
-                   ${user.transactionType}
-                   ${user.category} 
-                   ${user.description}
-                   ${user.amount}
-                   ${user.from}
-                   ${user.to}
-                   </li>
-                  `);
+        $('#th-id').append(`<tr><td>${user.id}</td></tr>`);
+        $('#th-username').append(`<tr><td>${user.username}</td></tr>`);
+        $('#th-transaction-type').append(`<tr><td>${user.transactionType}</td></tr>`);
+        $('#th-category').append(`<tr><td>${user.category}</td></tr>`);
+        $('#th-Description').append(`<tr><td>${user.description}</td></tr>`);
+        $('#th-amount').append(`<tr><td>${user.amount}</td></tr>`);
+        $('#th-from').append(`<tr><td>${user.from}</td></tr>`);
+        $('#th-to').append(`<tr><td>${user.to}</td></tr>`);
+
       });
     });
-  
+
     function getAcoountSumary() {
-  
+
     }
-  
-  
-  
-  
+
+
+
+
+
+
+
   });
 
 
