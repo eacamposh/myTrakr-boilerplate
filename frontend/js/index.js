14f8a088def03a7ca9e742437aea7ec900b70cfc
eacamposh <eacamposh@gmail.com>
2022-07-10 19:27:51 -0700

    add method for obtaining the summary of accounts

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -131,14 +131,14 @@ $(document).ready(function () {
   function showSelected(e) {
     console.log(e);
     if (this.checked) {
-      if (this.id === 'input-withdraw' || this.id === 'input-deposit' ) {
+      if (this.id === 'input-withdraw' || this.id === 'input-deposit') {
 
         console.log('with or deposit');
         $('select[name=select-from]').attr("disabled", true);
         $('select[name=select-to]').attr("disabled", true);
         $('select[name=select-account]').attr("disabled", false);
-      } 
-      else if(this.id === 'input-transfer' ) {
+      }
+      else if (this.id === 'input-transfer') {
         console.log('ELSE:');
         $('select[name=select-from]').attr("disabled", false);
         $('select[name=select-to]').attr("disabled", false);
@@ -149,93 +149,107 @@ $(document).ready(function () {
 
     }
   }
- 
- 
-    //? Eduardo's Code
-  
-    getNewAccounts();
-  
-  
-    $('#btn-add-new-account').on('click', function (e) {
-      let new_account = $('#new-account').val();
-      if (new_account.length == 0) {
-        alert("Empty field!");
-      }
-      else {
-        console.log('Else:', new_account);
-        $('#select-account').append('<option>' + new_account + '</option>');
-        $('#select-from').append('<option>' + new_account + '</option>');
-        $('#select-to').append('<option>' + new_account + '</option>');
-        $('#select-filter-by-account').append('<option>' + new_account + '</option>');
-  
-        $.ajax({
-          method: 'post',
-          data: JSON.stringify({
-            newAccount: {
-              username: `${new_account}`,
-              transactions: [],
-            },
-          }),
-          url: 'http://localhost:3000/accounts',
-          contentType: 'application/json',
-          dataType: 'json',
-        }).done((data) => {
-          console.log('Account Saved', data);
-        });
-      }
-    });
-  
-    function getNewAccounts() {
-      let new_account = "Select-Option";
+
+
+  //? Eduardo's Code
+
+  getNewAccounts();
+
+
+  $('#btn-add-new-account').on('click', function (e) {
+    let new_account = $('#new-account').val();
+    if (new_account.length == 0) {
+      alert("Empty field!");
+    }
+    else {
+      console.log('Else:', new_account);
+      $('#select-account').append('<option>' + new_account + '</option>');
+      $('#select-from').append('<option>' + new_account + '</option>');
+      $('#select-to').append('<option>' + new_account + '</option>');
+      $('#select-filter-by-account').append('<option>' + new_account + '</option>');
+
       $.ajax({
-        method: 'get',
+        method: 'post',
+        data: JSON.stringify({
+          newAccount: {
+            username: `${new_account}`,
+            transactions: [],
+          },
+        }),
         url: 'http://localhost:3000/accounts',
+        contentType: 'application/json',
         dataType: 'json',
       }).done((data) => {
-        console.log('data get ajax', data);
-        $('#select-account').empty();
-        $('#select-from').empty();
-        $('#select-to').empty();
-        $('#select-filter-by-account').empty();
-        console.log('Select-account', new_account);
-  
-        $('#select-account').append('<option>' + new_account + '</option>');
-        $('#select-from').append('<option>' + new_account + '</option>');
-        $('#select-to').append('<option>' + new_account + '</option>');
-        $('#select-filter-by-account').append('<option>' + new_account + '</option>');
-  
-  
-        $.each(data, (i, post) => {
-          console.log('each: ', post.username);
-          $('#select-account').append('<option>' + post.username + '</option>');
-          $('#select-from').append('<option>' + post.username + '</option>');
-          $('#select-to').append('<option>' + post.username + '</option>');
-          $('#select-filter-by-account').append('<option>' + post.username + '</option>');
-  
-        });
+        console.log('Account Saved', data);
       });
-  
     }
-  
-    //getJSON - file, cb(data)
-    $.getJSON('data.json', (data) => {
-      console.log('data json', data);
-      $.each(data, (index, user) => {
-        $('#th-id').append(`<tr><td>${user.id}</td></tr>`);
-        $('#th-username').append(`<tr><td>${user.username}</td></tr>`);
-        $('#th-transaction-type').append(`<tr><td>${user.transactionType}</td></tr>`);
-        $('#th-category').append(`<tr><td>${user.category}</td></tr>`);
-        $('#th-Description').append(`<tr><td>${user.description}</td></tr>`);
-        $('#th-amount').append(`<tr><td>${user.amount}</td></tr>`);
-        $('#th-from').append(`<tr><td>${user.from}</td></tr>`);
-        $('#th-to').append(`<tr><td>${user.to}</td></tr>`);
+  });
+
+  function getNewAccounts() {
+    let new_account = "Select-Option";
+    $.ajax({
+      method: 'get',
+      url: 'http://localhost:3000/accounts',
+      dataType: 'json',
+    }).done((data) => {
+      console.log('data get ajax', data);
+      $('#select-account').empty();
+      $('#select-from').empty();
+      $('#select-to').empty();
+      $('#select-filter-by-account').empty();
+      console.log('Select-account', new_account);
+
+      $('#select-account').append('<option>' + new_account + '</option>');
+      $('#select-from').append('<option>' + new_account + '</option>');
+      $('#select-to').append('<option>' + new_account + '</option>');
+      $('#select-filter-by-account').append('<option>' + new_account + '</option>');
+
+
+      $.each(data, (i, post) => {
+        console.log('each: ', post.username);
+        $('#select-account').append('<option>' + post.username + '</option>');
+        $('#select-from').append('<option>' + post.username + '</option>');
+        $('#select-to').append('<option>' + post.username + '</option>');
+        $('#select-filter-by-account').append('<option>' + post.username + '</option>');
 
       });
     });
-  
-    function getAcoountSumary() {
-  
-    }
+
+  }
+
+  //getJSON - file, cb(data)
+  $.getJSON('data.json', (data) => {
+    console.log('data json', data);
+    $.each(data, (index, user) => {
+      $('#th-id').append(`<tr><td>${user.id}</td></tr>`);
+      $('#th-username').append(`<tr><td>${user.username}</td></tr>`);
+      $('#th-transaction-type').append(`<tr><td>${user.transactionType}</td></tr>`);
+      $('#th-category').append(`<tr><td>${user.category}</td></tr>`);
+      $('#th-Description').append(`<tr><td>${user.description}</td></tr>`);
+      $('#th-amount').append(`<tr><td>${user.amount}</td></tr>`);
+      $('#th-from').append(`<tr><td>${user.from}</td></tr>`);
+      $('#th-to').append(`<tr><td>${user.to}</td></tr>`);
+
+    });
+  });
+
+
+  $('#btn-account-sumary').on('click', function (e) {
+    getAcoountSumary();
+  });
+
+
+  function getAcoountSumary() {
+    $.getJSON('sumary.json', (data) => {
+      console.log('data json', data);
+      $.each(data, (index, user) => {
+        $('#list').append(`
+        <li>${user.id} ${user.username} ${user.total}</li>
+        `);
+      });
+    });
+
+  }
 
 
 });
