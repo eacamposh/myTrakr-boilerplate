c6636b31f9a0f699e0b38bfa4722cd74a95ce6e3
eacamposh <eacamposh@gmail.com>
2022-07-12 21:09:31 -0700

    add validated new account

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -149,7 +149,7 @@ $(document).ready(function () {
 
     }
   }
-//btn sumbit the transaction.
+  //btn sumbit the transaction.
   $("#btn-add-transaction").on("click", function () {
 
     if (!$('#input-amount').val()) {
@@ -471,34 +471,48 @@ $(document).ready(function () {
 
   $('#btn-add-new-account').on('click', function (e) {
     let new_account = $('#new-account').val();
+
     if (new_account.length == 0) {
       alert("Empty field!");
-    }
-    else {
-      console.log('Else:', new_account);
-
-      // $('#select-account').append('<option>' + new_account + '</option>');
-      // $('#select-from').append('<option>' + new_account + '</option>');
-      // $('#select-to').append('<option>' + new_account + '</option>');
-      // $('#select-filter-by-account').append('<option>' + new_account + '</option>');
-
+    } else {
 
       $.ajax({
-        method: 'post',
-        data: JSON.stringify({
-          newAccount: {
-            username: `${new_account}`,
-            transactions: [],
-          },
-        }),
+        method: 'get',
         url: 'http://localhost:3000/accounts',
-        contentType: 'application/json',
         dataType: 'json',
       }).done((data) => {
-        console.log('Account Saved', data);
+        console.log('data get ajax', data);
+        let aux = 0;
+
+        $.each(data, (i, post) => {
+          console.log('New Account: ', post.username);
+          if (post.username === new_account) {
+            aux = aux + 1;
+
+          }
+        });
+        if (aux > 0) {
+          $("#errorMessage").text('Account already exist!');
+        } else {
+          console.log('Send post:', new_account);
+          $.ajax({
+            method: 'post',
+            data: JSON.stringify({
+              newAccount: {
+                username: `${new_account}`,
+                transactions: [],
+              },
+            }),
+            url: 'http://localhost:3000/accounts',
+            contentType: 'application/json',
+            dataType: 'json',
+          }).done((data) => {
+            console.log('Account Saved', data);
+          });
+
+        }
       });
     }
-
 
     getNewAccounts();
 
@@ -517,7 +531,6 @@ $(document).ready(function () {
       $('#select-from').empty();
       $('#select-to').empty();
       $('#select-filter-by-account').empty();
-      console.log('Select-account', new_account);
 
       $('#select-account').append('<option>' + new_account + '</option>');
       $('#select-from').append('<option>' + new_account + '</option>');
@@ -526,7 +539,7 @@ $(document).ready(function () {
 
 
       $.each(data, (i, post) => {
-        console.log('each: ', post.username);
+        console.log('New Account: ', post.username);
         $('#select-account').append('<option>' + post.id + " " + post.username + '</option>');
         $('#select-from').append('<option>' + post.id + " " + post.username + '</option>');
         $('#select-to').append('<option>' + post.id + " " + post.username + '</option>');
@@ -537,21 +550,20 @@ $(document).ready(function () {
 
   }
 
-  //getJSON - file, cb(data)
-  $.getJSON('data.json', (data) => {
-    console.log('data json', data);
-    $.each(data, (index, user) => {
-      $('#th-id').append(`<tr><td>${user.id}</td></tr>`);
-      $('#th-username').append(`<tr><td>${user.username}</td></tr>`);
-      $('#th-transaction-type').append(`<tr><td>${user.transactionType}</td></tr>`);
-      $('#th-category').append(`<tr><td>${user.category}</td></tr>`);
-      $('#th-Description').append(`<tr><td>${user.description}</td></tr>`);
-      $('#th-amount').append(`<tr><td>${user.amount}</td></tr>`);
-      $('#th-from').append(`<tr><td>${user.from}</td></tr>`);
-      $('#th-to').append(`<tr><td>${user.to}</td></tr>`);
-
-    });
-  });
+  /*   //getJSON - file, cb(data)
+    $.getJSON('data.json', (data) => {
+      $.each(data, (index, user) => {
+        $('#th-id').append(`<tr><td>${user.id}</td></tr>`);
+        $('#th-username').append(`<tr><td>${user.username}</td></tr>`);
+        $('#th-transaction-type').append(`<tr><td>${user.transactionType}</td></tr>`);
+        $('#th-category').append(`<tr><td>${user.category}</td></tr>`);
+        $('#th-Description').append(`<tr><td>${user.description}</td></tr>`);
+        $('#th-amount').append(`<tr><td>${user.amount}</td></tr>`);
+        $('#th-from').append(`<tr><td>${user.from}</td></tr>`);
+        $('#th-to').append(`<tr><td>${user.to}</td></tr>`);
+  
+      });
+    }); */
 
 
   $('#btn-account-sumary').on('click', function (e) {
@@ -563,9 +575,7 @@ $(document).ready(function () {
     $.getJSON('sumary.json', (data) => {
       console.log('data json', data);
       $.each(data, (index, user) => {
-        $('#list').append(`
-        <li>${user.id} ${user.username} ${user.total}</li>
-        `);
+        $('#list').append(`<li>${user.id} ${user.username} ${user.total}</li>`);
       });
     });
 
